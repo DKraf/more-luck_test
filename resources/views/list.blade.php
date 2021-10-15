ff2860689ff7e671ce2046120169ba6df12750a8
DKraf <dkraf9006@gmail.com>
2021-10-15 12:28:45 +0600

    #3

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -9,6 +9,7 @@
             <th scope="col">Имя</th>
             <th scope="col">Фото</th>
             <th scope="col">Тип</th>
+            <th scope="col">Дата загрузки</th>
             <th scope="col">Код</th>
             <th scope="col">Статус</th>
         </tr>
@@ -20,8 +21,18 @@
                 <td>{{ $data->name }}</td>
                 <td>{{ $data->photo }}</td>
                 <td>{{ $data->type }}</td>
-                <td>{{ $data->code }}</td>
-                <td>{{ $data->status }}</td>
+{{--                <td>{{$data->created_at}}</td>--}}
+                <td><?php echo ($data->created_at)->format('m-d-Y'); ?></td>
+                <td>@if(isset($data->code) && $data->code !='')
+                        <?php
+                            if (strtotime("+7 day",strtotime($data->created_at)) > strtotime(date("Y-m-d H:i:s"))) {
+                                echo $data->code;
+                            } else {
+                                echo 'Не учавствует на этой неделе' . date("Y-m-d H:i:s", strtotime("+7 day",strtotime($data->created_at)));
+                            }
+                        ?>
+                    @else {{ $data->code }} @endif</td>
+                <td> @if( $data->status  == 0) Отклонен @else Принят @endif</td>
 
             </tr>
         @endforeach
